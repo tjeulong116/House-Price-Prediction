9d993f9788c567a7e4b1add5b2c6a27860c6526a
tjeulong116 <tjeulong116@gmail.com>
2026-07-26 15:48:42 +0700

    Refactor authentication and prediction logic
    Refactor authentication and registration logic, and update data loading and prediction functions.

--- main.py
+++ main.py
@@ -9,36 +9,36 @@ from joblib import load
 import streamlit_authenticator as stauth
 from streamlit_authenticator.utilities.hasher import Hasher
 
-if "pred" not in st.session_state:
-    st.session_state["pred"] = None
+# if "pred" not in st.session_state:
+#     st.session_state["pred"] = None
 
-with open("config.yaml") as file:
-    config = yaml.load(stream=file, Loader=SafeLoader)
+# with open("config.yaml") as file:
+#     config = yaml.load(stream=file, Loader=SafeLoader)
 
-Hasher.hash_passwords(credentials=config["credentials"])
+# Hasher.hash_passwords(credentials=config["credentials"])
 
-authenticator = stauth.Authenticate(
-    credentials=config["credentials"],
-    cookie_name=config["cookie"]["name"],
-    cookie_key=config["cookie"]["key"],
-    cookie_expiry_days=config["cookie"]["expiry_days"]
-)
+# authenticator = stauth.Authenticate(
+#     credentials=config["credentials"],
+#     cookie_name=config["cookie"]["name"],
+#     cookie_key=config["cookie"]["key"],
+#     cookie_expiry_days=config["cookie"]["expiry_days"]
+# )
 
-authenticator.login(location="main")
-st.divider()
-with st.expander("Register"):
-    try:
-        if authenticator.register_user(location="main", clear_on_submit=True):
-            st.success('User registered successfully!')
+# authenticator.login(location="main")
+# st.divider()
+# with st.expander("Register"):
+#     try:
+#         if authenticator.register_user(location="main", clear_on_submit=True):
+#             st.success('User registered successfully!')
 
-            for username, data in config['credentials']['usernames'].items():
-                if 'roles' in data and data['roles'] is None:
-                    del data['roles']
+#             for username, data in config['credentials']['usernames'].items():
+#                 if 'roles' in data and data['roles'] is None:
+#                     del data['roles']
 
-            with open('config.yaml', 'w') as file:
-                yaml.dump(config, file, default_flow_style=False)
-    except Exception as e:
-        st.error(e)
+#             with open('config.yaml', 'w') as file:
+#                 yaml.dump(config, file, default_flow_style=False)
+#     except Exception as e:
+#         st.error(e)
 
 
 @st.cache_data
@@ -102,50 +102,92 @@ def display_chart(df: pd.DataFrame):
 
     st.pyplot(fig)
 
-if st.session_state["authentication_status"]:
-    authenticator.logout(location="sidebar")
-    st.title("House price calculator")
+# if st.session_state["authentication_status"]:
+#     authenticator.logout(location="sidebar")
+#     st.title("House price calculator")
 
-    df = read_data()
-    reg = load_model()
+#     df = read_data()
+#     reg = load_model()
 
-    with st.form(key="form"):
-        col1, col2, col3 = st.columns(3)
+#     with st.form(key="form"):
+#         col1, col2, col3 = st.columns(3)
 
-        with col1:
-            st.number_input(label="Diện tích", min_value=1.0, value=50.0, step=0.1, key="area")
-            st.number_input(label="Tầng", min_value=1, value=5, step=1, key="stories")
-            st.selectbox(label="Quận/Huyện", index=0, key="district",
-                         options=['Tây Hồ', 'Hà Đông', 'Ba Đình', 'Hoàn Kiếm', 'Thanh Trì', 'Cầu Giấy',
-                                 'Hoài Đức', 'Thanh Xuân', 'Nam Từ Liêm', 'Hoàng Mai', 'Long Biên', 'Đống Đa',
-                                 'Đông Anh', 'Thanh Oai', 'Hai Bà Trưng', 'Gia Lâm', 'Bắc Từ Liêm', 'Chương Mỹ',
-                                 'Quốc Oai', 'Thường Tín', 'Phúc Thọ', 'Đan Phượng', 'Ứng Hòa', 'Mê Linh',
-                                 'Mỹ Đức', 'Sóc Sơn', 'Thạch Thất'])
+#         with col1:
+#             st.number_input(label="Diện tích", min_value=1.0, value=50.0, step=0.1, key="area")
+#             st.number_input(label="Tầng", min_value=1, value=5, step=1, key="stories")
+#             st.selectbox(label="Quận/Huyện", index=0, key="district",
+#                          options=['Tây Hồ', 'Hà Đông', 'Ba Đình', 'Hoàn Kiếm', 'Thanh Trì', 'Cầu Giấy',
+#                                  'Hoài Đức', 'Thanh Xuân', 'Nam Từ Liêm', 'Hoàng Mai', 'Long Biên', 'Đống Đa',
+#                                  'Đông Anh', 'Thanh Oai', 'Hai Bà Trưng', 'Gia Lâm', 'Bắc Từ Liêm', 'Chương Mỹ',
+#                                  'Quốc Oai', 'Thường Tín', 'Phúc Thọ', 'Đan Phượng', 'Ứng Hòa', 'Mê Linh',
+#                                  'Mỹ Đức', 'Sóc Sơn', 'Thạch Thất'])
 
-        with col2:
-            st.number_input(label="Mặt tiền", min_value=1.0, value=4.5, step=0.1, key="front")
-            st.number_input("Phòng ngủ", min_value=1, value=4, step=1, key="bedrooms")
-        with col3:
-            st.selectbox("Pháp lý", options=['Sổ đỏ', 'Không có sổ'], index=0, key="legal")
-            st.number_input("Phòng tắm", min_value=1, value=4, key="bathrooms")
+#         with col2:
+#             st.number_input(label="Mặt tiền", min_value=1.0, value=4.5, step=0.1, key="front")
+#             st.number_input("Phòng ngủ", min_value=1, value=4, step=1, key="bedrooms")
+#         with col3:
+#             st.selectbox("Pháp lý", options=['Sổ đỏ', 'Không có sổ'], index=0, key="legal")
+#             st.number_input("Phòng tắm", min_value=1, value=4, key="bathrooms")
 
-        st.form_submit_button("Tính toán", type="primary", on_click=make_prediction, kwargs=dict(model=reg))
+#         st.form_submit_button("Tính toán", type="primary", on_click=make_prediction, kwargs=dict(model=reg))
 
-    if st.session_state["pred"] is not None:
-        st.subheader(f"Giá ước lượng cho căn nhà trên là {st.session_state.pred} tỷ VND")
-    else:
-        st.write("Nhập thông tin và nhấn nút Tính toán để có ước lượng giá căn nhà")
+#     if st.session_state["pred"] is not None:
+#         st.subheader(f"Giá ước lượng cho căn nhà trên là {st.session_state.pred} tỷ VND")
+#     else:
+#         st.write("Nhập thông tin và nhấn nút Tính toán để có ước lượng giá căn nhà")
 
-    st.write("\n------------------------------------------------------\n")
-    with st.expander(label="Xem dữ liệu"):
-        st.dataframe(df)
+#     st.write("\n------------------------------------------------------\n")
+#     with st.expander(label="Xem dữ liệu"):
+#         st.dataframe(df)
 
-    st.write("\n------------------------------------------------------\n")
-    st.subheader("Columns Visualization")
-    st.selectbox("Chọn cột", options=["Giá", "Diện tích", "Giá/m2", "Tầng", "Phòng ngủ", "Phòng tắm"], index=0, key="column_visualization")
-    display_chart(df)
+#     st.write("\n------------------------------------------------------\n")
+#     st.subheader("Columns Visualization")
+#     st.selectbox("Chọn cột", options=["Giá", "Diện tích", "Giá/m2", "Tầng", "Phòng ngủ", "Phòng tắm"], index=0, key="column_visualization")
+#     display_chart(df)
 
-elif st.session_state["authentication_status"] == False:
-    st.error("Nhập sai tài khoản hoặc mật khẩu")
-elif st.session_state["authentication_status"] is None:
-    pass
+# elif st.session_state["authentication_status"] == False:
+#     st.error("Nhập sai tài khoản hoặc mật khẩu")
+# elif st.session_state["authentication_status"] is None:
+#     pass
+
+authenticator.logout(location="sidebar")
+st.title("House price calculator")
+
+df = read_data()
+reg = load_model()
+
+with st.form(key="form"):
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.number_input(label="Diện tích", min_value=1.0, value=50.0, step=0.1, key="area")
+        st.number_input(label="Tầng", min_value=1, value=5, step=1, key="stories")
+        st.selectbox(label="Quận/Huyện", index=0, key="district",
+                     options=['Tây Hồ', 'Hà Đông', 'Ba Đình', 'Hoàn Kiếm', 'Thanh Trì', 'Cầu Giấy',
+                             'Hoài Đức', 'Thanh Xuân', 'Nam Từ Liêm', 'Hoàng Mai', 'Long Biên', 'Đống Đa',
+                             'Đông Anh', 'Thanh Oai', 'Hai Bà Trưng', 'Gia Lâm', 'Bắc Từ Liêm', 'Chương Mỹ',
+                             'Quốc Oai', 'Thường Tín', 'Phúc Thọ', 'Đan Phượng', 'Ứng Hòa', 'Mê Linh',
+                             'Mỹ Đức', 'Sóc Sơn', 'Thạch Thất'])
+
+    with col2:
+        st.number_input(label="Mặt tiền", min_value=1.0, value=4.5, step=0.1, key="front")
+        st.number_input("Phòng ngủ", min_value=1, value=4, step=1, key="bedrooms")
+    with col3:
+        st.selectbox("Pháp lý", options=['Sổ đỏ', 'Không có sổ'], index=0, key="legal")
+        st.number_input("Phòng tắm", min_value=1, value=4, key="bathrooms")
+
+    st.form_submit_button("Tính toán", type="primary", on_click=make_prediction, kwargs=dict(model=reg))
+
+if st.session_state["pred"] is not None:
+    st.subheader(f"Giá ước lượng cho căn nhà trên là {st.session_state.pred} tỷ VND")
+else:
+    st.write("Nhập thông tin và nhấn nút Tính toán để có ước lượng giá căn nhà")
+
+st.write("\n------------------------------------------------------\n")
+with st.expander(label="Xem dữ liệu"):
+    st.dataframe(df)
+
+st.write("\n------------------------------------------------------\n")
+st.subheader("Columns Visualization")
+st.selectbox("Chọn cột", options=["Giá", "Diện tích", "Giá/m2", "Tầng", "Phòng ngủ", "Phòng tắm"], index=0, key="column_visualization")
+display_chart(df)
